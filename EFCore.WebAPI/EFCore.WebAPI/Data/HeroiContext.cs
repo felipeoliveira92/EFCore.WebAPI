9faa52850d659bb5446dadaf8b047092214d7d29
felipeoliveira92 <61589016+felipeoliveira92@users.noreply.github.com>
2021-09-08 22:03:58 -0300

    criado os models, migrations and data

---
+++ EFCore.WebAPI/EFCore.WebAPI/Data/HeroiContext.cs
@@ -0,0 +1,30 @@
+﻿using EFCore.WebAPI.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCore.WebAPI.Data
+{
+    public class HeroiContext : DbContext
+    {
+        public DbSet<Heroi> Herois { get; set; }
+        public DbSet<Batalha> Batalhas { get; set; }
+        public DbSet<Arma> Armas { get; set; }
+        public DbSet<HeroiBatalha> HeroisBatalhas { get; set; }
+        public DbSet<IdentidadeSecreta> identidadeSecretas { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Password=Alarmaq2014@;Persist Security Info=True;User ID=sa;Initial Catalog=HeroApp;Data Source=DESKTOP-FELIPE");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<HeroiBatalha>(entity => 
+            {
+                entity.HasKey(e => new { e.BatalhaId, e.HeroiId });                
+            });
+        }
+    }
+}
